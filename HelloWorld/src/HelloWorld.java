c584d6fb19a3e4f8bead532d3f5ce33b33fb4d74
Penn Song <penn.song@channelwin.info>
2011-07-19 08:23:07 +0800

    master branch1

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world!!");
 		System.out.println("push to upstream test123456");
-
+		System.out.println("master branch1");
 	}
 
 }
